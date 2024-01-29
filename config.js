4e1f8821c8ec1284bab97f8d2f1a4576205e23d1
PsychoIII <155296122+PsychoIII@users.noreply.github.com>
2024-01-29 23:41:38 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['+260 978119791', 'YORAM MN', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
